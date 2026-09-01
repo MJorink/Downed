bc6af225b5cabe99709184cd52d0ee9d92bc3089
Jorink <maxjorink@gmail.com>
2026-09-01 22:56:09 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using downed;
 
 [assembly: MelonInfo(typeof(Downed), "Downed", Downed.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(255,0,255,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,255,0,255)]
+[assembly: MelonAuthorColor(255,255,0,0)]
